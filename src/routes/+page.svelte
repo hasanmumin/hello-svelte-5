9a4b8bf82c415203222bc51380dedfa64ba1e37e
hasanmumin <0hasanmumin@gmail.com>
2024-10-23 11:09:07 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h2>What's in this kit?</h2>
+<ul>
+	<li>
+		<a href="/demo/lucia">Lucia</a>
+	</li>
+</ul>
